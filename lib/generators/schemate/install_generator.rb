93102dc144ea75431b1ff798f435d3e5fafd72f4
eishi <saito@eishis.com>
2018-03-20 00:41:15 +0900

    Add auto generated config file

--- lib/generators/schemate/install_generator.rb
+++ lib/generators/schemate/install_generator.rb
@@ -3,36 +3,16 @@ require 'securerandom'
 module Schemate
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path("../templates", __FILE__)
 
-      desc "Creates schemate initializer, routes and copy locale
-files to your application."
-      class_option :orm
 
-      def copy_initializer
-        @underscored_user_name = "user".underscore
+      desc 'Copy annotate_models rakefiles for automatic annotation'
+      source_root File.expand_path('templates', __dir__)
+
+      # copy rake tasks
+      def copy_tasks
+        template 'schemate.rb', 'config/initializers/schemate.rb'
       end
 
-      def install_assets
-        require 'rails'
-      end
-
-      def setup_routes
-        route "mount Schemate::Engine => '/'"
-      end
-
-      def self.source_root
-        File.expand_path("../templates", __FILE__)
-      end 
-
-      def create_migrations
-        Dir["#{self.class.source_root}/migrations/*.rb"].sort.each do
-|filepath|
-          name = File.basename(filepath)
-          template "migrations/#{name}", "db/migrate/#{name}"
-          sleep 1
-        end
-      end
     end
   end
 end
---
+++ lib/generators/schemate/templates/schemate.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+Schemate.setup do |config|
+  config.auto_generate = false
+end
