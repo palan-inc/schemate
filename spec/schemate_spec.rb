fc7ca039b88ac9a8f4fd37c586f11411439a65aa
Ryosuke Kakehashi <kakepeer@gmail.com>
2018-06-20 17:27:48 +0900

    Add csv_row spec.

--- spec/schemate_spec.rb
+++ spec/schemate_spec.rb
@@ -1,11 +1,54 @@
 require "spec_helper"
+require 'schemate'
 
-RSpec.describe Schemate do
+describe Schemate do
   it "has a version number" do
     expect(Schemate::VERSION).not_to be nil
   end
 
   it "does something useful" do
     expect(false).to eq(true)
+  end
+
+  def mock_class(table_name, primary_key, columns, indexes = [], foreign_keys = [])
+    options = {
+      connection:       mock_connection(indexes, foreign_keys),
+      table_exists?:    true,
+      table_name:       table_name,
+      primary_key:      primary_key,
+      column_names:     columns.map { |col| col.name.to_s },
+      columns:          columns,
+      column_defaults:  Hash[columns.map { |col| [col.name, col.default] }],
+      table_name_prefix: ''
+    }
+
+    double('An ActiveRecord class', options)
+  end
+
+  def mock_connection(indexes = [], foreign_keys = [])
+    double('Conn',
+           indexes:      indexes,
+           foreign_keys: foreign_keys,
+           supports_foreign_keys?: true)
+  end
+
+  def mock_column(name, type, options = {})
+    default_options = {
+      limit: nil,
+      null: false,
+      default: nil
+    }
+
+    stubs = default_options.dup
+    stubs.merge!(options)
+    stubs[:name] = name
+    stubs[:type] = type
+
+    double('Column', stubs)
+  end
+
+  it do
+    column = mock_column(:id, :integer, limit: 8)
+    expect(Schemate.csv_row(column 'true')).to eq("id integer 8 true nil true nil")
   end
 end
