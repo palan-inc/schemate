67e07f03d20ab0905d2d227eba377ff154ca6bc4
eishi <saito@eishis.com>
2018-06-27 17:29:27 +0900

    edit order with colveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,8 @@
-require "bundler/setup"
-require "schemate"
 require 'coveralls'
 Coveralls.wear!
+
+require "bundler/setup"
+require "schemate"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
